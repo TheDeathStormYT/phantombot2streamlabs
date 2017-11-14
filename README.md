67d491ccd4dfc7cf71ff068aa9f8443adf6ff80c
h4ckninja <h4ckninja@users.noreply.github.com>
2017-11-13 23:13:43 -0800

    New tool!

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # phantombot2streamlabs
+
 Export Phantombot points to Streamlabs.
+
+Streamlabs has points features that are easy to use for streamers. However, many streamers, myself included, use Phantombot for our commands, points, and more.
+
+If you are looking to move out of Phantombot to Streamlabs for your points, here's a tool!
+
+
+Requirements:
+- Python 3 installed (https://www.python.org/ if you do not already have it installed for your OS)
+
+
+Instructions:
+- Put `export_points.py` in your Phantombot folder. This is the same folder as `Phantombot.jar`.
+- On the command line (`cmd.exe` for Windows, `Terminal` for macOS), enter `python export_points.py` and press enter.
+- If it was successful, you'll see the following message: `[+] Success! You can upload the points-export.csv file to streamlabs (https://streamlabs.com/dashboard#/accountsettings)!`
---
+++ export_points.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import csv
+import sqlite3
+import pathlib
+
+
+def run(database):
+	conn = sqlite3.connect(database)
+	conn.row_factory = dict_factory
+	cursor = conn.cursor()
+
+	pointsCursor = cursor.execute('SELECT variable, value FROM phantombot_points')
+	points_rows = pointsCursor.fetchall()
+
+	# Write the CSV
+	with open('points-export.csv', 'w+') as csvfile:
+		points_writer = csv.writer(csvfile, delimiter = ',')
+
+		points_writer.writerow(['', '', ''])
+
+		for points in points_rows:
+			points_writer.writerow([str(points['variable']), str(points['variable']), str(points['value'])])
+
+
+	conn.close()
+
+	print('[+] Success! You can upload the points-export.csv file to streamlabs (https://streamlabs.com/dashboard#/accountsettings)!')
+
+
+# Shamelessly stolen from https://stackoverflow.com/questions/3300464/how-can-i-get-dict-from-sqlite-query/3300514#3300514
+def dict_factory(cursor, row):
+	d = {}
+
+	for idx, col in enumerate(cursor.description):
+		d[col[0]] = row[idx]
+
+	return d
+
+
+if __name__ == '__main__':
+	db = pathlib.Path('./phantombot.db')
+
+	if not db.exists():
+		print('Could not find phantombot database.')
+		print('If you use MySQL for phantombot, please comment on https://github.com/h4ckninja/phantombot2streamlabs/issues/1')
+
+	else:
+		db = str(db)
+		run(db)
